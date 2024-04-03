289828a5be5586640db35e43e3644f288d519fc7
kapilahuja-iris <165653388+kapilahuja-iris@users.noreply.github.com>
2024-04-03 06:13:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,51 +1,24 @@
-// Create Web Server
-// 1. Load libraries
-const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
+// Create Web Server and listen to port 3000
+// Create a route for /comments
+// When user visits /comments, return a json object with a list of comments
 
-// 2. Create an instance of express
+const express = require("express");
 const app = express();
 
-// 3. Configure the express instance
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// 4. Connect to the database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'comment_system'
+app.get("/comments", (req, res) => {
+  res.json({
+    comments: [
+      { username: "Alice", comment: "I love apples!" },
+      { username: "Bob", comment: "Where is the apple tree?" },
+      { username: "Eve", comment: "I want to eat an apple." }
+    ]
+  });
 });
 
-connection.connect();
-
-// 5. Create a route
-app.get('/comments', (req, res) => {
-    // 6. Query the database
-    connection.query('SELECT * FROM comments', (error, results) => {
-        // 7. Send the results back as JSON
-        res.json(results);
-    });
-});
-
-app.post('/comments', (req, res) => {
-    // 8. Get the data from the request
-    const comment = req.body.comment;
-
-    // 9. Insert the data into the database
-    connection.query('INSERT INTO comments (comment) VALUES (?)', [comment], (error, results) => {
-        // 10. Send a response back to the client
-        res.json({
-            id: results.insertId,
-            comment: comment
-        });
-    });
-});
-
-// 11. Start the server
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  console.log("Server is listening on port 3000");
 });
+
+// Run this file using node comments.js
+// Visit http://localhost:3000/comments in your browser
+// You should see a json object with a list of comments
