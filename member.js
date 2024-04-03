812d575873ef03aad188aaa8ccc68ce8c0e1d63c
kapilahuja-iris <165653388+kapilahuja-iris@users.noreply.github.com>
2024-04-03 05:46:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMembers() {
+  var members = [
+    { name: 'John', skills: ['JavaScript', 'HTML', 'CSS'] },
+    { name: 'Alice', skills: ['Python', 'SQL'] },
+    { name: 'Kate', skills: ['PHP', 'AWS'] }
+  ];
+
+  return members;
+}
